a28e62a88634fda943b98198f1a15a7833394bcc
dina aid <dinaaid3@gmail.com>
2018-12-10 22:35:40 +0200

    version0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from "@angular/fire/database";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -7,24 +10,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('updatedMemberX') updatedMemberX: ElementRef;
+
+  membersRef: AngularFireList<any>;
+
   editMemberX: boolean = false;
   editId: number;
-  updatedMember: string;
+  updatedMember: string = '';
 
   newMemberX: string = "";
 
-  members: string[] = ['Ahmed', 'Sarah', 'Dina'];
+  members$: any;
 
-  editMember(i, updatedMember) {
-    this.editMemberX = true;
-    this.editId = i;
+  constructor(public db: AngularFireDatabase) {
+    this.membersRef = db.list('members');
+    // Use snapshotChanges().map() to store the key
+    this.members$ = this.membersRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
   }
 
   addMember(newMember) {
-    this.members.unshift(newMember);   
+    this.membersRef.push({ name: this.newMemberX });
+    this.newMemberX = '';
   }
 
-  removeMember(i) {
-    this.members.splice(i, 1);
+  removeMember(key: string) {
+    if(confirm('R u sure u wanna delete this?!'))
+      this.membersRef.remove(key);
+  }
+
+  editMember(key: string, newText: string) {
+    this.editMemberX = false;
+    // setTimeout( () => this.updatedMemberX.nativeElement.focus() );
+    // console.log(newText);
+    this.membersRef.update(key, { name: newText });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdlBNMD013feSxJShOYxIYI9gdZBr5nhw",
+  authDomain: "pharmacydinax.firebaseapp.com",
+  databaseURL: "https://pharmacydinax.firebaseio.com",
+  projectId: "pharmacydinax",
+  storageBucket: "pharmacydinax.appspot.com",
+  messagingSenderId: "847108860094"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +26,9 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
